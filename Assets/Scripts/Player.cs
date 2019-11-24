70156fbf99c00287811e25f300910a9cddf898d8
Marc Sevilleja <marc@sevilayha.com>
2019-11-23 21:54:02 -0800

    Movement constraints for player
    Implemented player constraints using Mathf.Clamp and the ViewportToWorldPoint with reference to the camera. Added padding so player does not move through the boundary due to the pivot being in the center of the sprite

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,14 +7,24 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+
+    float xMin, xMax, yMin, yMax;
     
-    // Start is called before the first frame update
     void Start()
     {
-        
+        setUpMoveBoundaries();          // set up boundaries of camera which player can move in
     }
 
-    // Update is called once per frame
+    private void setUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;     // what is world space value of x element
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
     void Update()
     {
         Move();    
@@ -22,9 +33,10 @@ public class Player : MonoBehaviour
     private void Move()
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
-        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * (moveSpeed * .75f);
-        var newYPos = transform.position.y + deltaY;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * (moveSpeed * .5f);
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
 
         transform.position = new Vector2(newXPos, newYPos);
     }
