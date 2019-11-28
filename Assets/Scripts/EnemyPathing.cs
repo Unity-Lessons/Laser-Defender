abf2c22241427acffc99833918f792f06cecb561
Marc Sevilleja <marc@sevilayha.com>
2019-11-28 00:44:46 -0800

    Enemy Pathing implemented
    Enemy moves towards predetermined waypoints using the Vector2.MoveTowards method.

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -6,16 +6,31 @@ public class EnemyPathing : MonoBehaviour
 {
 
     [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 1.5f;
+    int waypointIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = waypoints[waypointIndex].position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (waypointIndex < waypoints.Count)
+        {
+            var targetPosition = waypoints[waypointIndex].position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(
+                    transform.position,
+                    targetPosition,
+                    movementThisFrame);
+            if (transform.position == targetPosition)
+                waypointIndex++;
+        }
+        else
+            Destroy(gameObject);
+
     }
 }
