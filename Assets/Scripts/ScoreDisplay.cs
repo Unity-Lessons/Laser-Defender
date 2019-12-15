0938538b4ba6a43d320761996b49ee0b662aeac7
Marc Sevilleja <marc@sevilayha.com>
2019-12-15 14:03:33 -0800

    Added ways to keep track of and print score to screen

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    Text scoreText;
+    GameSession gameSession;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        gameSession = GetComponent<GameSession>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.Text = gameSession.GetScore().ToString();
+    }
+}
